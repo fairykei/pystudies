b1e74c0be3eb8bacb9e068674c312cf2457a4604
Letícia Souza <kjjs0927@gmail.com>
2019-11-20 22:28:26 -0300

    day two so far !!

---
+++ src/30dayschallenge/d2.py
@@ -0,0 +1,15 @@
+# Day 2: 30 Days of python programming
+
+#Declaring vars in just one line
+first_name, last_name, country, age, is_married = 'Asabeneh', 'Yetayeh', 'Helsink', 250, True
+print(first_name, last_name, country, age, is_married)
+
+
+# Complex numbers
+print('Complex number: ', 1+1j)
+print('Multiplying complex number: ', (1+1j) * (1-1j))
+
+# exercises !!
+
+print("\n# ")
+print()
