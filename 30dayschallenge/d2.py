2f584cf6f16375b63419b34aa683226b0d81b4d1
Letícia Souza <kjjs0927@gmail.com>
2019-11-21 00:29:59 -0300

    my d2 is broken >:(

--- 30dayschallenge/d2.py
+++ 30dayschallenge/d2.py
@@ -1,9 +1,5 @@
 # Day 2: 30 Days of python programming
 
-#Declaring vars in just one line
-first_name, last_name, country, age, is_married = 'Asabeneh', 'Yetayeh', 'Helsink', 250, True
-print(first_name, last_name, country, age, is_married)
-
 
 # Complex numbers
 print('Complex number: ', 1+1j)
@@ -11,5 +7,84 @@ print('Multiplying complex number: ', (1+1j) * (1-1j))
 
 # exercises !!
 
-print("\n# ")
-print()
+print("\n# 1")
+first_name = "leticia"
+
+print("\n# 2")
+last_name = "souza"
+
+print("\n# 3")
+full_name = first_name, " ", last_name
+
+print("\n# 4")
+country = "brazil"
+
+print("\n# 5")
+city = "sao paulo"
+
+print("\n# 6")
+age = 20
+
+print("\n# 7")
+year = 2019
+
+print("\n# 8")
+is_married = False
+
+print("\n# 9")
+is_true = True
+
+print("\n# 10")
+is_light_on = True
+
+print("\n# 11")
+# Declaring vars in just one line
+dog_name, breed, age = 'Andy', 'Pinscher Doberman', 2.5
+
+print("\n# 12")
+print(type(first_name))
+print(type(last_name))
+print(type(full_name))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light_on))
+print(type(dog_name))
+print(type(breed))
+print(type(age))
+
+print("\n# 13")
+print(len(first_name))
+
+print("\n# 14")
+print("bigger length is: ", max(len(first_name), len(last_name)))
+
+print("\n# 15")
+num_one, num_two = 5, 4
+total = num_one + num_two
+diff = num_one - num_two
+product = num_two * num_one
+division = num_one / num_two
+remainder = num_two % num_one
+exp = num_one ** num_two
+floor_division = num_one // num_two
+
+print("\n# 16")
+radius = 30
+area_of_circle = radius * 3.14
+circum_of_circle = 3.14 * (radius*2)
+
+radius = input('insert the circle radius: ')
+area_of_circle = radius * 3.14
+
+print("\n# 17")
+first_name = input('first name: ')
+last_name = input('last name: ')
+country = input('country: ')
+age = input('age: ')
+
+print("\n# 18")
+help('keywords')
