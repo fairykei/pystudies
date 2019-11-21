15f9204ad25087079db5283cbfbf4e8d0fd724ab
Leticia Souza <kjjs0927@gmail.com>
2019-11-21 13:14:43 -0300

    hjgkj

--- src/cotton.py
+++ src/cotton.py
@@ -1,16 +1,22 @@
 # this is a comment !
 def main():
     print("this is basically what i did in my holiday")
-    listao = ["sdasd", "dsfdsfads", "dfdsfads", 10, ["list inside a list", 12], 50]
+    listao = ["sdasd", "dsfdsfads", "dfdsfads",
+              10, ["list inside a list", 12], 50]
     texto = "dfjadsfois"
     nro = 10.5
     bigger = 0
-    smaller = None
-    for index in range(10):
-        x = input("enter a number pls")
-        if x < smaller or smaller == None:
+    smaller = 0
+    x = 0
+    print(listao)
+    print(texto)
+    print(nro)
+
+    for i in range(10):
+        x = input("enter a number pls: ")
+        if smaller == 0 or x < smaller:
             smaller = x
-        if x > bigger:
+        elif int(x) > bigger:
             bigger = x
 
-    print("bigger number: {0} \nsmaller number: {1}".format(bigger, smaller))
+    print("biggest number: {0} \nsmaller number: {1}".format(bigger, smaller))
